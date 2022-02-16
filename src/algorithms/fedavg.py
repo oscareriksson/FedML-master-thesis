b9c550b253d13f28d72517ca2d9ddbc7f839e83c
oscareriksson <oscareriksson1@hotmail.com>
2022-02-16 16:34:14 +0100

    FedProx and refactoring

---
+++ src/algorithms/fedavg.py
@@ -0,0 +1,68 @@
+from .server_base import ServerBase
+import copy
+import torch.optim as optim
+from collections import OrderedDict
+import torch.nn.functional as F
+
+
+class FedAvgServer(ServerBase):
+    def __init__(self, args, train_loaders, test_loader) -> None:
+        super().__init__(args, train_loaders, test_loader)
+
+        self.local_model = None
+        self.n_clients = args.n_clients
+        self.n_rounds = args.n_rounds
+        self.lr_rate = args.learning_rate
+        self.local_epochs = args.local_epochs
+        self.n_samples_client = [len(data_loader.dataset) for data_loader in train_loaders]
+        self.n_samples_total = sum(self.n_samples_client)
+
+    
+    def _loss_function(self, output, labels, epoch):
+        return F.cross_entropy(output, labels)
+
+    def run(self):
+
+        # Initialize average weights to 0
+        avg_weights = OrderedDict()
+        global_weights = self.global_model.state_dict()
+        for param_name in self.global_model.state_dict().keys():
+            avg_weights[param_name] = global_weights[param_name] * 0
+        
+        for j in range(self.n_clients):
+            print("-- Client {} --".format(j+1), flush=True)
+            self._local_training(j)
+
+            avg_weights = self._increment_weighted_average(avg_weights, self.local_model.state_dict(), j)
+
+            self.set_weights(avg_weights)    
+    
+    def _local_training(self, client_nr):
+        self.local_model = copy.deepcopy(self.global_model)
+        self.local_model.train()
+        optimizer = optim.SGD(self.local_model.parameters(), lr=self.lr_rate)
+
+        for i in range(self.local_epochs):
+            for x, y in self.train_loaders[client_nr]:
+                optimizer.zero_grad()
+                output = self.local_model(x)
+                error = self._loss_function(output, y, i)
+                error.backward()
+                optimizer.step()
+
+    def _increment_weighted_average(self, model, model_next, client_nr):
+        """ Update an incremental average. """
+        w = OrderedDict()
+        for name in model.keys():
+            #tensorDiff = model_next[name] - model[name]
+            w[name] = model[name] + model_next[name] * self.get_scaling_factor(client_nr)
+        return w
+    
+    def get_scaling_factor(self, client_nr):
+        return self.n_samples_client[client_nr] / self.n_samples_total
+
+    def get_weights(self):
+        return self.model.get_weights()
+    
+    def set_weights(self, weights):
+        self.global_model.load_state_dict(weights)
---
+++ src/algorithms/server_base.py
@@ -0,0 +1,40 @@
+from ..models.models import create_model
+from abc import ABC, abstractmethod
+import torch
+
+class ServerBase(ABC):
+    """Abstract base class defining datasets."""
+    def __init__(self, args, train_loaders, test_loader) -> None:
+        self.global_model = create_model(args.model_name)
+        self.train_loaders = train_loaders
+        self.test_loader = test_loader
+        
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def _local_training(self, client_nr):
+        pass
+
+    @abstractmethod
+    def _loss_function(output, labels, epoch):
+        pass
+
+    def evaluate(self):
+        self.global_model.eval()
+        test_losses = []
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in self.test_loader:
+                output = self.global_model(data)
+                test_loss += self._loss_function(output, target, 1).item()
+                _, pred = torch.max(output.data, 1)
+                correct += (pred == target).sum().item()
+        test_loss /= len(self.test_loader.dataset)
+        test_losses.append(test_loss)
+        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(self.test_loader.dataset),
+        100. * correct / len(self.test_loader.dataset)))
