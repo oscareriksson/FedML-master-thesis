669d22db4634d481798ca4479a622aa4835b085e
oscareriksson <oscareriksson1@hotmail.com>
2022-02-17 14:57:16 +0100

    Dataset refactoring and added support for GPU

--- src/algorithms/server_base.py
+++ src/algorithms/server_base.py
@@ -5,7 +5,8 @@ import torch
 class ServerBase(ABC):
     """Abstract base class defining datasets."""
     def __init__(self, args, train_loaders, test_loader) -> None:
-        self.global_model = create_model(args.model_name)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.global_model = create_model(args.model_name).to(self.device)
         self.train_loaders = train_loaders
         self.test_loader = test_loader
         
@@ -28,11 +29,12 @@ class ServerBase(ABC):
         test_loss = 0
         correct = 0
         with torch.no_grad():
-            for data, target in self.test_loader:
-                output = self.global_model(data)
-                test_loss += self._loss_function(output, target, 1).item()
+            for x, y in self.test_loader:
+                x, y = x.to(self.device), y.to(self.device)
+                output = self.global_model(x)
+                test_loss += self._loss_function(output, y, 1).item()
                 _, pred = torch.max(output.data, 1)
-                correct += (pred == target).sum().item()
+                correct += (pred == y).sum().item()
         test_loss /= len(self.test_loader.dataset)
         test_losses.append(test_loss)
         print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
--- src/datasets/mnist.py
+++ src/datasets/mnist.py
@@ -1,15 +1,11 @@
-from .dataset_base import DatasetBase
-from torchvision.datasets import MNIST
+from .dataset_base import PytorchDataset
 from torchvision.transforms import Compose, ToTensor, Normalize
-from torch.utils.data import DataLoader, Subset
-import torch
-import numpy as np
 
-
-class Mnist(DatasetBase):
+class Mnist(PytorchDataset):
     """ Mnist dataset class.
     """
     def __init__(self, train_fraction):
+        super().__init__("MNIST")
         """ Constructor method.
 
             Parameters:
@@ -17,7 +13,7 @@ class Mnist(DatasetBase):
         """
 
         transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
-        self.test_data = MNIST(
+        self.test_data = self.data_class(
             root='data', 
             train=False, 
             transform=transform, 
@@ -25,82 +21,3 @@ class Mnist(DatasetBase):
 
         self.train_data = self._sample_train_data(train_fraction, transform)
         self.n_samples = len(self.train_data)
-
-    def _sample_train_data(self, train_fraction, transform):
-        """ Sample a chosen fraction of the training dataset to use.
-
-            Parameters:
-            train_fraction  (float): Fraction of training data to use.
-            transform       (torchvision.transforms.Compose): Collection of transforms to apply on data.
-
-            Returns:
-            torch.utils.data.Subset: Subset of training data.
-        """
-        train_data = MNIST(
-            root='data', 
-            train=True, 
-            transform=transform, 
-            download=True)
-        n_samples = len(train_data.targets)
-        index_limit = int(train_fraction * n_samples)
-        chosen_indices = np.random.choice(torch.arange(n_samples), size=index_limit, replace=False)
-        print(f"\nUsing {index_limit} training samples\n", flush=True)
-
-        return Subset(train_data, chosen_indices)
-
-    def get_train_data_loaders(self, n_clients, distribution, alpha, batch_size):
-        """ Get list of client training data loaders sampled from dirichlet distribution.
-
-            Parameters:
-            n_clients       (int): Number of clients.
-            distribution    (str): iid/non-iid distributed data.
-            alpha           (float): Concentration parameter for dirichlet distribution.
-            batch_size      (int): Batch size for loading training data.
-
-            Returns List[torch.utils.data.DataLoader]
-        """
-        labels = np.array([y for (_, y) in self.train_data])
-        n_classes = len(np.unique(labels))
-        partition_matrix = np.ones((n_classes, n_clients))
-
-        # iid TODO: Make random slices.
-        if distribution == "iid":
-            partition_matrix /= n_clients
-            size = int(np.floor(len(labels)/n_clients))
-            client_data_loaders = []
-            client_indices = np.random.choice(torch.arange(self.n_samples), size=(n_clients, size), replace=False)
-            for i in range(n_clients):
-                client_data_loaders.append(DataLoader(Subset(self.train_data, client_indices[i]), batch_size))
-        # non-iid
-        else:
-            class_indices = []
-            for i in range(10):
-                class_indices.append(np.array(range(len(labels)))[labels == i])
-            valid_pm = False
-            while not valid_pm:
-                partition_matrix = np.random.dirichlet((alpha, )*n_clients, n_classes)
-                valid_pm = all(np.sum(partition_matrix, axis=0) > 0.01)
-
-            local_sets_indices = [[] for _ in range(n_clients)]
-            for each_class in range(n_classes):
-                sample_size = len(class_indices[each_class])
-                for client in range(n_clients):
-                    np.random.shuffle(class_indices[each_class])
-                    local_size = int(np.floor(partition_matrix[each_class, client] * sample_size))
-                    local_sets_indices[client] += list(class_indices[each_class][:local_size])
-                    class_indices[each_class] = class_indices[each_class][local_size:]
-
-            client_data_loaders = []
-            for client_indices in local_sets_indices:
-                np.random.shuffle(client_indices)
-                client_data_loaders.append(DataLoader(Subset(self.train_data, client_indices), batch_size))
-
-        return partition_matrix, client_data_loaders
-    
-    def get_test_data_loader(self, batch_size):
-        """ Get test data loader.
-
-            Parameters:
-            batch_size      (int): Batch size for loading test data.
-        """
-        return DataLoader(self.test_data, batch_size)
