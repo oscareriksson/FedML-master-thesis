72021283361719ee872b55bfbc0e7cb4ebcf20c3
oscareriksson <oscareriksson1@hotmail.com>
2022-02-18 14:58:06 +0100

    Progress bars, evaluation during training and minor changes for fedprox loss

--- src/algorithms/server_base.py
+++ src/algorithms/server_base.py
@@ -1,15 +1,27 @@
 from ..models.models import create_model
 from abc import ABC, abstractmethod
 import torch
+import torch.nn as nn
+import copy
 
 class ServerBase(ABC):
     """Abstract base class defining datasets."""
-    def __init__(self, args, train_loaders, test_loader) -> None:
+    def __init__(self, args, model, train_loaders, test_loader) -> None:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.global_model = create_model(args.model_name).to(self.device)
+        self.global_model = copy.deepcopy(model).to(self.device)
         self.train_loaders = train_loaders
         self.test_loader = test_loader
-        
+        self.local_model = None
+        self.n_clients = args.n_clients
+        self.n_rounds = args.n_rounds
+        self.lr_rate = args.learning_rate
+        self.momentum = args.momentum
+        self.local_epochs = args.local_epochs
+        self.loss_function = nn.CrossEntropyLoss()
+        self.n_samples_client = [len(data_loader.dataset) for data_loader in train_loaders]
+        self.n_samples_total = sum(self.n_samples_client)
+        self.evaluate_train = args.evaluate_train
+        self.round_nr = 0
 
     @abstractmethod
     def run(self):
@@ -19,11 +31,7 @@ class ServerBase(ABC):
     def _local_training(self, client_nr):
         pass
 
-    @abstractmethod
-    def _loss_function(output, labels, epoch):
-        pass
-
-    def evaluate(self):
+    def test(self):
         self.global_model.eval()
         test_losses = []
         test_loss = 0
@@ -32,11 +40,14 @@ class ServerBase(ABC):
             for x, y in self.test_loader:
                 x, y = x.to(self.device), y.to(self.device)
                 output = self.global_model(x)
-                test_loss += self._loss_function(output, y, 1).item()
+                test_loss += self.loss_function(output, y).item()
                 _, pred = torch.max(output.data, 1)
                 correct += (pred == y).sum().item()
         test_loss /= len(self.test_loader.dataset)
         test_losses.append(test_loss)
-        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(self.test_loader.dataset),
-        100. * correct / len(self.test_loader.dataset)))
+        print('\nGlobal Model Test: Avg. loss: {:.4f}, Accuracy: {:.0f}%\n'.format(
+            test_loss,
+            100. * correct / len(self.test_loader.dataset)))
+        
+    def set_round(self, round_nr):
+        self.round_nr = round_nr
