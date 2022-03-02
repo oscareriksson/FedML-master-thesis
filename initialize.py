70ef9204d90a2fc1529c6689b1b16c33f66c7b36
oscareriksson <oscareriksson1@hotmail.com>
2022-03-02 07:49:02 +0100

    Load settings and save results

---
+++ initialize.py
@@ -0,0 +1,66 @@
+import argparse
+import importlib
+import os
+from src.models.models import create_model
+import torch
+import numpy as np
+import string
+import random
+
+
+def prepare_settings_folder(path_name):
+    while True:
+        settings_folder = f"{path_name}_{''.join(random.choice(string.ascii_lowercase) for _ in range(3))}"
+        if not os.path.exists(settings_folder):
+            os.makedirs(settings_folder)
+            break
+    return settings_folder
+
+def initialize_data(args):
+    assert args.dataset in ["mnist", "cifar10", "cifar100"], f"Chosen dataset is not available."
+    module = importlib.import_module(f"src.datasets.{args.dataset}")
+    data_class_ = getattr(module, args.dataset.title())
+
+    dataset = data_class_(args.train_fraction)
+    dataset.generate_client_data(args.n_clients, args.distribution, args.alpha)
+    local_indices = dataset.get_local_sets_indices()
+
+    dataset.split_test_public(args.n_samples_public)
+    test_indices, public_indices = dataset.get_test_indices(), dataset.get_public_indices()
+
+    return local_indices, test_indices, public_indices
+
+
+
+def main(args):
+    path_name = f"./settings/{args.dataset}_c{args.n_clients}_f{args.train_fraction}_{args.distribution}_a{args.alpha}_npub{args.n_samples_public}"
+
+    settings_folder = prepare_settings_folder(path_name)
+    
+    init_model = create_model(args.model_name)
+    torch.save(init_model, f"{settings_folder}/model")
+    
+    local_indices, test_indices, public_indices = initialize_data(args)
+
+    with open(f'{settings_folder}/data_splits.npy', 'wb') as f:
+        np.save(f, test_indices)
+        np.save(f, public_indices)
+        for indices in local_indices:
+            np.save(f, indices)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="mnist")
+    parser.add_argument("--model_name", type=str, default="mnist_cnn")
+    parser.add_argument("--n_clients", type=int, default=5, help="Number of clients per round")
+    parser.add_argument("--train_fraction", type=float, default=0.1)
+    parser.add_argument("--distribution", type=str, default="niid")
+    parser.add_argument("--alpha", type=float, default=0.1)
+
+    # Ensemble parameters
+    parser.add_argument("--n_samples_public", type=int, default=1000)
+
+    args = parser.parse_args()
+
+    main(args)
--- main.py
+++ main.py
@@ -1,42 +1,58 @@
 import argparse
+from cProfile import run
 import importlib
 import sys
+import os
 import torch
-from src.models.models import create_model
+import numpy as np
+import string
+import random
+
+def prepare_run_folder():
+    while True:
+        run_folder = f"./results/run_{''.join(random.choice(string.ascii_lowercase) for _ in range(3))}"
+        if not os.path.exists(run_folder):
+            os.makedirs(run_folder)
+            break
+    return run_folder
+
+def save_run_settings(args, run_folder):
+    with open(f"{run_folder}/run_settings", "w") as f:
+        for arg, value in vars(args).items():
+            f.write(f"{arg} {value}")
+            f.write("\n")
 
 
-def load_data_loaders(args):
+def load_data_loaders(args, local_indices, test_indices, public_indices):
     assert args.dataset in ["mnist", "cifar10", "cifar100"], f"Chosen dataset is not available."
     module = importlib.import_module(f"src.datasets.{args.dataset}")
     data_class_ = getattr(module, args.dataset.title())
 
-    dataset = data_class_(args.train_fraction)
-    dataset.generate_client_data(args.n_clients, args.distribution, args.alpha)
+    dataset = data_class_()
 
-    public_data_loader = None
-    algs = args.algorithm.split("+")
-    ens_algs = ["feded"]
-    if any(alg in algs for alg in ens_algs):
-        dataset.split_test_public(args.n_samples_public)
-        public_data_loader = dataset.get_public_data_loader(args.public_batch_size)
+    dataset.set_local_sets_indices(local_indices)
+    dataset.set_test_indices(test_indices)
+    dataset.set_public_indices(public_indices)
 
     client_data_loaders = dataset.get_train_data_loaders(args.train_batch_size)
     test_data_loader = dataset.get_test_data_loader(args.test_batch_size)
+    public_data_loader = dataset.get_public_data_loader(args.public_batch_size)
 
     return client_data_loaders, test_data_loader, public_data_loader
 
-def create_server(alg, args, model, client_loaders, test_loader, public_loader):
-    if alg == "fedavg":
+
+def create_server(args, model, client_loaders, test_loader, public_loader, run_folder):
+    if args.algorithm == "fedavg":
         from src.algorithms.fedavg import FedAvgServer
         server = FedAvgServer(args, model, client_loaders, test_loader)
 
-    elif alg == "fedprox":
+    elif args.algorithm == "fedprox":
         from src.algorithms.fedprox import FedProxServer
         server = FedProxServer(args, model, client_loaders, test_loader)
 
-    elif alg == "feded":
+    elif args.algorithm == "feded":
         from src.algorithms.feded import FedEdServer
-        server = FedEdServer(args, model, client_loaders, test_loader, public_loader)
+        server = FedEdServer(args, model, client_loaders, test_loader, public_loader, run_folder)
         
     else:
         print("Chosen algorithm is not supported.")
@@ -45,39 +61,42 @@ def create_server(alg, args, model, client_loaders, test_loader, public_loader):
     return server
 
 
-def run_job(args, i):
-    torch.manual_seed(i)
-    client_data_loaders, test_data_loader, public_data_loader = load_data_loaders(args)
-    model = create_model(args.model_name)
-
-    algorithms = args.algorithm.split("+")
-    for alg in algorithms:
-        print(alg.upper(), flush=True)
-        server = create_server(alg, args, model, client_data_loaders, test_data_loader, public_data_loader)
-
-        server.run()
-
-
-
 def main(args):
-    for i in range(args.n_times):
-        print(f"_________ Iteration {i+1} _________ ", flush=True)
-        run_job(args, i)
+    run_folder = prepare_run_folder()
+    save_run_settings(args, run_folder)
+
+    settings_path = f"./settings/{args.settings_file}"
+    model = torch.load(f"{settings_path}/model")
+
+    local_indices = []
+    with open(f'{settings_path}/data_splits.npy', 'rb') as f:
+        test_indices = np.load(f)
+        public_indices = np.load(f)
+        try:
+            while True:
+                local_indices.append(np.load(f))
+        except:
+            print("End of file.")
+
+
+    client_data_loaders, test_data_loader, public_data_loader = load_data_loaders(args, local_indices, test_indices, public_indices)
+
+    server = create_server(args, model, client_data_loaders, test_data_loader, public_data_loader, run_folder)
+    server.run()
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="cifar10")
-    parser.add_argument("--algorithm", type=str, default="feded")
-    parser.add_argument("--n_clients", type=int, default=2, help="Number of clients per round")
+    parser.add_argument("--settings_file", type=str, default="mnist_c5_f0.1_niid_a0.1_npub1000_dks")
+    parser.add_argument("--dataset", type=str, default="mnist")
+    parser.add_argument("--algorithm", type=str, default="fedavg")
+    parser.add_argument("--n_clients", type=int, default=5, help="Number of clients per round")
     parser.add_argument("--n_times", type=int, default=1)
-    parser.add_argument("--n_rounds", type=int, default=1)
+    parser.add_argument("--n_rounds", type=int, default=5)
     parser.add_argument("--local_epochs", type=int, default=1)
-    parser.add_argument("--train_fraction", type=float, default=0.05)
-    parser.add_argument("--distribution", type=str, default="iid")
-    parser.add_argument("--alpha", type=float, default=0.1)
+    parser.add_argument("--train_fraction", type=float, default=0.1)
     parser.add_argument("--mu", type=float, default=0.0)
-    parser.add_argument("--model_name", type=str, default="cifar10_cnn")
     parser.add_argument("--train_batch_size", type=int, default=64)
     parser.add_argument("--test_batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=0.001, help="Local learning rate")
@@ -89,7 +108,7 @@ if __name__ == "__main__":
     parser.add_argument("--n_samples_public", type=int, default=1000)
     parser.add_argument("--public_batch_size", type=int, default=64)
     parser.add_argument("--student_batch_size", type=int, default=16)
-    parser.add_argument("--student_epochs", type=int, default=60)
+    parser.add_argument("--student_epochs", type=int, default=10)
 
     args = parser.parse_args()
 
