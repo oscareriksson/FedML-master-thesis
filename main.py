ebcbe02e1fafc9a69a6be18dda082b9a7fdfedba
oscareriksson <oscareriksson1@hotmail.com>
2022-03-07 11:33:20 +0100

    More public samples

--- main.py
+++ main.py
@@ -89,23 +89,22 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--settings_file", type=str, default="mnist_c5_f0.1_iid_a0.1_qgn")
+    parser.add_argument("--settings_file", type=str, default="mnist_c10_f1.0_niid_a0.1_qki")
     parser.add_argument("--algorithm", type=str, default="feded")
     parser.add_argument("--n_rounds", type=int, default=1)
-    parser.add_argument("--local_epochs", type=int, default=3)
+    parser.add_argument("--local_epochs", type=int, default=1)
     parser.add_argument("--mu", type=float, default=0.0)
     parser.add_argument("--train_batch_size", type=int, default=64)
     parser.add_argument("--test_batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=0.001, help="Local learning rate")
     parser.add_argument("--momentum", type=float, default=0.9, help="Local momentum")
-    parser.add_argument("--evaluate_train", type=bool, default=True, help="Do evaluation of local training")
 
     # Ensemble parameters
-    parser.add_argument("--local_epochs_ensemble", type=int, default=5)
+    parser.add_argument("--local_epochs_ensemble", type=int, default=30)
     parser.add_argument("--public_batch_size", type=int, default=64)
     parser.add_argument("--student_batch_size", type=int, default=64)
-    parser.add_argument("--student_epochs", type=int, default=15)
-    parser.add_argument("--public_data_sizes", type=str, default="1000 3000 5000")
+    parser.add_argument("--student_epochs", type=int, default=150)
+    parser.add_argument("--public_data_sizes", type=str, default="1000 3000 5000 7000")
 
     args = parser.parse_args()
 
