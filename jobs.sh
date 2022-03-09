48ac76655b6a47f1414ed00b7f152155b2e8fef9
oscareriksson <oscareriksson1@hotmail.com>
2022-03-09 11:35:43 +0100

    Cifar jobs

--- jobs.sh
+++ jobs.sh
@@ -6,11 +6,11 @@
 # python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_txx --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
 # python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_udi --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
 
-python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_fyd --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_kne --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_mhg --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yhm --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_fyd --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_kne --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_mhg --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yhm --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
 
 # python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_ooy --algorithm fedavg --local_epochs 1 --n_rounds 50
 # python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_oup --algorithm fedavg --local_epochs 1 --n_rounds 50
@@ -24,11 +24,8 @@ python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm feded -
 # python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yhm --algorithm fedavg --local_epochs 1 --n_rounds 50
 # python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm fedavg --local_epochs 1 --n_rounds 50
 
-git config --global user.email "oscareriksson1@hotmail.com"
-git config --global user.name "Oscar Eriksson"
-
-git add -f results/
-git commit -m "Test results, weighted"
-git push
-
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm feded --local_epochs_ensemble 1 --n_rounds 1 --student_epochs 10
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_eft --algorithm fedavg --local_epochs 1 --n_rounds 150
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_gmg --algorithm fedavg --local_epochs 1 --n_rounds 150
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_mfb --algorithm fedavg --local_epochs 1 --n_rounds 150
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_xmo --algorithm fedavg --local_epochs 1 --n_rounds 150
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm fedavg --local_epochs 1 --n_rounds 150
