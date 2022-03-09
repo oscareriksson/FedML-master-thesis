b11becfb032f91ff18430cc9ef80dea0387fb406
oscareriksson <oscareriksson1@hotmail.com>
2022-03-09 14:07:37 +0100

    Cifar student model

--- src/models/models.py
+++ src/models/models.py
@@ -84,7 +84,7 @@ class Cifar_Student(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, 5)
         self.pool2 = nn.MaxPool2d(2)
         self.conv3 = nn.Conv2d(32, 64, 5)
-        self.pool3 = nn.MaxPool2d(2)
+
         self.fc1 = nn.Linear(64 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, n_classes)
@@ -92,6 +92,7 @@ class Cifar_Student(nn.Module):
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
