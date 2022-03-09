e78d3c21c1fd6a1f62afbd142a0fbb9d7ec5cd5e
oscareriksson <oscareriksson1@hotmail.com>
2022-03-09 16:18:05 +0100

    Cifar iid feded jobs

--- jobs.sh
+++ jobs.sh
@@ -30,8 +30,8 @@
 # python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_xmo --algorithm fedavg --local_epochs 1 --n_rounds 150
 # python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm fedavg --local_epochs 1 --n_rounds 150
 
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_eft --algorithm feded --local_epochs_ensemble 70 1 --n_rounds 1 --student_epochs 1000
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_gmg --algorithm feded --local_epochs_ensemble 70 1 --n_rounds 1 --student_epochs 1000
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_mfb --algorithm feded --local_epochs_ensemble 70 1 --n_rounds 1 --student_epochs 1000
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_xmo --algorithm feded --local_epochs_ensemble 70 1 --n_rounds 1 --student_epochs 1000
-python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm feded --local_epochs_ensemble 70 1 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_eft --algorithm feded --local_epochs_ensemble 70 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_gmg --algorithm feded --local_epochs_ensemble 70 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_mfb --algorithm feded --local_epochs_ensemble 70 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_xmo --algorithm feded --local_epochs_ensemble 70 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm feded --local_epochs_ensemble 70 --n_rounds 1 --student_epochs 1000
