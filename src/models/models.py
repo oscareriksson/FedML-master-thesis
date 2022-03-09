acfdf38bba028cc2cae2059c31e7e6faec3cdc7e
oscareriksson <oscareriksson1@hotmail.com>
2022-03-09 14:15:08 +0100

    Cifar student model

--- src/models/models.py
+++ src/models/models.py
@@ -79,20 +79,26 @@ class Mnist_Student(nn.Module):
 class Cifar_Student(nn.Module):
     def __init__(self, n_classes):
         super(Cifar_Student, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.conv1 = nn.Conv2d(3, 16, 5, 1, 2)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.conv2 = nn.Conv2d(16, 32, 5, 1, 2)
         self.pool2 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(32, 64, 5, 1, 2)
+        self.pool3 = nn.MaxPool2d(2)
 
-        self.fc1 = nn.Linear(64 * 5 * 5, 128)
+        self.fc1 = nn.Linear(64 * 4 * 4, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, n_classes)
 
+        self.dropout = nn.Dropout(0.2, inplace=False)
+
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool2(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
+        x = self.dropout(x)
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.dropout(x)
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
