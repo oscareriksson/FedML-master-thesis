d57fb57f440bc66cf9bac3d8490418eb50016b08
oscareriksson <oscareriksson1@hotmail.com>
2022-03-11 09:42:16 +0100

    Student model bug

--- src/models/models.py
+++ src/models/models.py
@@ -65,20 +65,23 @@ class Cifar_Cnn(nn.Module):
 class Mnist_Student(nn.Module):
     def __init__(self):
         super(Mnist_Student, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 5, 1, 2)
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(16, 32, 5, 1, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 64)
-        self.fc2 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(16 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
+
 
 
 class Cifar_Student(nn.Module):
