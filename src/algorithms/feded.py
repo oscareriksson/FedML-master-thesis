c79a092f987c4d65888dff341dbfb4a5338b9bab
oscareriksson <oscareriksson1@hotmail.com>
2022-03-11 10:25:00 +0100

    Changed student lr

--- src/algorithms/feded.py
+++ src/algorithms/feded.py
@@ -102,7 +102,7 @@ class FedEdServer(ServerBase):
         model = create_model(self.dataset_name, student=True)
         model.to(self.device)
         loss_function = nn.L1Loss()
-        optimizer = optim.Adam(model.parameters(), lr=0.01)
+        optimizer = optim.Adam(model.parameters(), lr=0.0001)
         train_accs, train_losses, val_accs, val_losses = [], [], [], []
         for epoch in range(self.student_epochs):
             model.train()   
