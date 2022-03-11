f0491f7bc1b06a2fb2354f5597d22e7ceecd3829
oscareriksson <oscareriksson1@hotmail.com>
2022-03-11 13:21:38 +0100

    Cifar preparations

--- jobs.sh
+++ jobs.sh
@@ -1,43 +1,43 @@
 #!/bin/sh
 
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_ooy --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_oup --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_tqr --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_txx --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_udi --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+python3 main.py --settings_file mnist_c10_iid_a0.1_kfd --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c10_iid_a0.1_odl --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c10_iid_a0.1_rac --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c10_niid_a0.1_kbj --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c10_niid_a0.1_wwl --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c10_niid_a0.1_zja --algorithm fedavg --local_epochs 1 --n_rounds 100
 
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_fyd --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_kne --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_mhg --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yhm --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 150
+python3 main.py --settings_file mnist_c10_iid_a0.1_kfd --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
+python3 main.py --settings_file mnist_c10_iid_a0.1_odl --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
+python3 main.py --settings_file mnist_c10_iid_a0.1_rac --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
+python3 main.py --settings_file mnist_c10_niid_a0.1_kbj --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
+python3 main.py --settings_file mnist_c10_niid_a0.1_wwl --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
+python3 main.py --settings_file mnist_c10_niid_a0.1_zja --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000
 
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_ooy --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_oup --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_tqr --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_txx --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_iid_a0.1_udi --algorithm fedavg --local_epochs 1 --n_rounds 50
+python3 main.py --settings_file mnist_c10_iid_a0.1_kfd --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
+python3 main.py --settings_file mnist_c10_iid_a0.1_odl --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
+python3 main.py --settings_file mnist_c10_iid_a0.1_rac --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
+python3 main.py --settings_file mnist_c10_niid_a0.1_kbj --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
+python3 main.py --settings_file mnist_c10_niid_a0.1_wwl --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
+python3 main.py --settings_file mnist_c10_niid_a0.1_zja --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 20000 30000 --weight_scheme w1
 
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_fyd --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_kne --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_mhg --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yhm --algorithm fedavg --local_epochs 1 --n_rounds 50
-# python3 main.py --settings_file mnist_c10_f1.0_niid_a0.1_yjr --algorithm fedavg --local_epochs 1 --n_rounds 50
+python3 main.py --settings_file cifar10_c10_iid_a0.1_cpp --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_c10_iid_a0.1_mwj --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_c10_iid_a0.1_zcv --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_c10_niid_a0.1_hop --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_c10_niid_a0.1_mpf --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_c10_niid_a0.1_tug --algorithm fedavg --local_epochs 1 --n_rounds 100
 
-# python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_eft --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
-# python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_gmg --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
-# python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_mfb --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
-# python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_xmo --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
-# python3 main.py --settings_file cifar10_c10_f1.0_iid_a0.1_ygi --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
+python3 main.py --settings_file cifar10_c10_iid_a0.1_cpp --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
+python3 main.py --settings_file cifar10_c10_iid_a0.1_mwj --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
+python3 main.py --settings_file cifar10_c10_iid_a0.1_zcv --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
+python3 main.py --settings_file cifar10_c10_niid_a0.1_hop --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
+python3 main.py --settings_file cifar10_c10_niid_a0.1_mpf --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
+python3 main.py --settings_file cifar10_c10_niid_a0.1_tug --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000
 
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_bqa --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000
-python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rff --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000 --num_workers 6
-python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rgy --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000 --num_workers 4
-python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rls --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000 --num_workers 4
-python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_sew --algorithm feded --local_epochs_ensemble 60 --n_rounds 1 --student_epochs 1000 --num_workers 4
-
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_bqa --algorithm fedavg --local_epochs 1 --n_rounds 150
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rff --algorithm fedavg --local_epochs 1 --n_rounds 150
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rgy --algorithm fedavg --local_epochs 1 --n_rounds 150
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_rls --algorithm fedavg --local_epochs 1 --n_rounds 150
-# python3 main.py --settings_file cifar10_c10_f1.0_niid_a0.1_sew --algorithm fedavg --local_epochs 1 --n_rounds 150
+python3 main.py --settings_file cifar10_c10_iid_a0.1_cpp --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
+python3 main.py --settings_file cifar10_c10_iid_a0.1_mwj --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
+python3 main.py --settings_file cifar10_c10_iid_a0.1_zcv --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
+python3 main.py --settings_file cifar10_c10_niid_a0.1_hop --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
+python3 main.py --settings_file cifar10_c10_niid_a0.1_mpf --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
+python3 main.py --settings_file cifar10_c10_niid_a0.1_tug --algorithm feded --local_epochs_ensemble 30 --n_rounds 1 --student_epochs 100 --public_data_sizes 1000 5000 10000 25000 --weight_scheme w1
