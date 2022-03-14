b25c2b3d12ae391c3f5b8792412ad119dab7698b
oscareriksson <oscareriksson1@hotmail.com>
2022-03-14 08:50:08 +0100

    Cifar model

--- src/models/models.py
+++ src/models/models.py
@@ -65,8 +65,7 @@ class Cifar_Cnn(nn.Module):
         x = self.pool3(F.relu(self.conv6(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 
