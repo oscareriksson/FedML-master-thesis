87755336f71af8cdf930b64b95c88d5dc976360a
oscareriksson <oscareriksson1@hotmail.com>
2022-03-14 13:49:17 +0100

    Stochastic training of student

--- src/datasets/dataset_student.py
+++ src/datasets/dataset_student.py
@@ -1,23 +1,19 @@
 from torch.utils.data import Dataset
 
 class StudentData(Dataset):
-    def __init__(self, dataset, targets):
+    def __init__(self, dataset):
         
         self.dataset = dataset
-        self.targets = targets
-        self.indices = dataset.indices
             
     def __getitem__(self, index):
         if isinstance(index, list):
             data, _ = self.dataset[[i for i in index]]
-            target = self.targets[[self.indices[i] for i in index]]
         else:
             data, _ = self.dataset[index]
-            target = self.targets[self.indices[index]]
             
-        return data, target
+        return data, index
         
     
     def __len__(self):
         """Total number of samples"""
-        return len(self.indices)
+        return len(self.dataset)
