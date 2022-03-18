819ff3500576589e01825f678ad75e244b7d6c39
oscareriksson <oscareriksson1@hotmail.com>
2022-03-18 08:27:09 +0100

    mnist runs

--- jobs.sh
+++ jobs.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-python3 main.py --settings_file mnist_c10_iid_a0.1_hrq --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file mnist_c10_iid_a0.1_suu --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file mnist_c10_iid_a0.1_zdr --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_iid_a0.1_hrq --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_iid_a0.1_suu --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_iid_a0.1_zdr --algorithm fedavg --local_epochs 1 --n_rounds 100
 
-python3 main.py --settings_file mnist_c10_niid_a0.1_jsc --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file mnist_c10_niid_a0.1_exf --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file mnist_c10_niid_a0.1_mjm --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_niid_a0.1_jsc --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_niid_a0.1_exf --algorithm fedavg --local_epochs 1 --n_rounds 100
+# python3 main.py --settings_file mnist_c10_niid_a0.1_mjm --algorithm fedavg --local_epochs 1 --n_rounds 100
+
+python3 main.py --settings_file mnist_c20_iid_a0.1_azo --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c20_iid_a0.1_jnl --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c20_iid_a0.1_ocy --algorithm fedavg --local_epochs 1 --n_rounds 100
+
+python3 main.py --settings_file mnist_c20_niid_a0.1_hjs --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c20_niid_a0.1_meb --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file mnist_c20_niid_a0.1_tfy --algorithm fedavg --local_epochs 1 --n_rounds 100
 
 # python3 main.py --settings_file mnist_c10_iid_a0.1_hrq --algorithm feded --local_epochs 20 --n_rounds 1 --public_data_sizes="500 1000 5000 15000 30000" --client_sample_fraction 0.4 --student_epochs 30
 # python3 main.py --settings_file mnist_c10_iid_a0.1_suu --algorithm feded --local_epochs 20 --n_rounds 1 --public_data_sizes="500 1000 5000 15000 30000" --client_sample_fraction 0.4 --student_epochs 30
