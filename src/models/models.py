a54a6629e7e93196a1b0a2290fdb10f22a53a150
oscareriksson <oscareriksson1@hotmail.com>
2022-03-21 11:21:48 +0100

    autoenc test

--- src/models/models.py
+++ src/models/models.py
@@ -38,21 +38,21 @@ class Mnist_Cnn2(nn.Module):
     def __init__(self):
         super(Mnist_Cnn2, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, 5, 1, 2)
-        self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(16, 32, 5, 1, 2)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(32, 64, 5, 1, 2)
         self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.drop = nn.Dropout()
-        self.fc3 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool1(F.relu(self.conv2(x)))
+        x = self.pool2(F.relu(self.conv3(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.drop(x)
         x = self.fc3(x)
         return x
 
