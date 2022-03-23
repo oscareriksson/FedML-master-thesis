eafdd5888df5725de88daa33f70b714cd010733a
oscareriksson <oscareriksson1@hotmail.com>
2022-03-23 17:11:30 +0100

    runs

--- cifar10_runs.sh
+++ cifar10_runs.sh
@@ -12,11 +12,11 @@
 # python3 main.py --settings_file cifar10_resnet_c10_niid0.1_kih --algorithm fedavg --local_epochs 1 --n_rounds 100
 # python3 main.py --settings_file cifar10_resnet_c10_niid0.1_sgk --algorithm fedavg --local_epochs 1 --n_rounds 100
 
-python3 main.py --settings_file cifar10_resnet_c10_iid_qre --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
-python3 main.py --settings_file cifar10_resnet_c10_iid_vvr --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
-python3 main.py --settings_file cifar10_resnet_c10_iid_yzu --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
-python3 main.py --settings_file cifar10_resnet_c10_iid_dpp --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
-python3 main.py --settings_file cifar10_resnet_c10_iid_xsf --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
+python3 main.py --settings_file cifar10_resnet_c10_iid_qre --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
+python3 main.py --settings_file cifar10_resnet_c10_iid_vvr --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
+python3 main.py --settings_file cifar10_resnet_c10_iid_yzu --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
+python3 main.py --settings_file cifar10_resnet_c10_iid_dpp --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
+python3 main.py --settings_file cifar10_resnet_c10_iid_xsf --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
 
 # python3 main.py --settings_file cifar10_resnet_c10_niid0.1_got --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
 # python3 main.py --settings_file cifar10_resnet_c10_niid0.1_hhk --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="100 500 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
