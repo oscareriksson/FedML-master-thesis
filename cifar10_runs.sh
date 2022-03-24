717003c050e98119f2cf64a4c08a311427c41586
oscareriksson <oscareriksson1@hotmail.com>
2022-03-24 07:56:19 +0100

    cifar runs

--- cifar10_runs.sh
+++ cifar10_runs.sh
@@ -6,11 +6,11 @@
 # python3 main.py --settings_file cifar10_resnet_c10_iid_dpp --algorithm fedavg --local_epochs 1 --n_rounds 100
 # python3 main.py --settings_file cifar10_resnet_c10_iid_xsf --algorithm fedavg --local_epochs 1 --n_rounds 100
 
-# python3 main.py --settings_file cifar10_resnet_c10_niid0.1_got --algorithm fedavg --local_epochs 1 --n_rounds 100
-# python3 main.py --settings_file cifar10_resnet_c10_niid0.1_hhk --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file cifar10_resnet_c10_niid0.1_zkc --algorithm fedavg --local_epochs 1 --n_rounds 100
-python3 main.py --settings_file cifar10_resnet_c10_niid0.1_kih --algorithm fedavg --local_epochs 1 --n_rounds 100
-# python3 main.py --settings_file cifar10_resnet_c10_niid0.1_sgk --algorithm fedavg --local_epochs 1 --n_rounds 100
+python3 main.py --settings_file cifar10_resnet_c10_niid0.1_got --algorithm fedavg --local_epochs 1 --n_rounds 100 --train_batch_size 100
+python3 main.py --settings_file cifar10_resnet_c10_niid0.1_hhk --algorithm fedavg --local_epochs 1 --n_rounds 100 --train_batch_size 100
+python3 main.py --settings_file cifar10_resnet_c10_niid0.1_zkc --algorithm fedavg --local_epochs 1 --n_rounds 100 --train_batch_size 100
+python3 main.py --settings_file cifar10_resnet_c10_niid0.1_kih --algorithm fedavg --local_epochs 1 --n_rounds 100 --train_batch_size 100
+python3 main.py --settings_file cifar10_resnet_c10_niid0.1_sgk --algorithm fedavg --local_epochs 1 --n_rounds 100 --train_batch_size 100
 
 # python3 main.py --settings_file cifar10_resnet_c10_iid_qre --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
 # python3 main.py --settings_file cifar10_resnet_c10_iid_vvr --algorithm feded --local_epochs_ensemble 50 --student_model cifar10_resnet --public_data_sizes="500 1000 5000 10000 25000" --client_sample_fraction 0.8 --student_epochs 15 --weight_scheme 0
