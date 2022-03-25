63a850b6f8039a8f11476dc1e528810f1bce94ad
oscareriksson <oscareriksson1@hotmail.com>
2022-03-25 08:48:36 +0100

    cifar runs

--- src/models/models.py
+++ src/models/models.py
@@ -90,6 +90,26 @@ class Emnist_Cnn2(nn.Module):
         x = self.fc3(x)
         return x
 
+class Cifar10_Cnn(nn.Module):
+    def __init__(self):
+        super(Cifar10_Cnn, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 5, 1, 2)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(16, 32, 5, 1, 2)
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 26)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 class Cifar10_Resnet(nn.Module):
     def __init__(self):
