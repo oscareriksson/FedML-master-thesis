73e39e926fa8818c4d8fac1ebfe069a6439824be
oscareriksson <oscareriksson1@hotmail.com>
2022-03-30 11:37:17 +0200

    mnist runs

--- init_settings.sh
+++ init_settings.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
 
-# alpha=( 10 1 0.1 0.01)
-# seed=( 1 2 3 4 5)
+alpha=( 10 1 0.1 0.01)
+seed=( 1 2 3 4 5)
 
-# for a in "${alpha[@]}"
-#     do
-#         for s in "${seed[@]}"
-#         do
-#             python3 initialize.py --seed $s --dataset mnist --model_name mnist_cnn1 --n_clients 10 --alpha $a
-#         done
-#     done
-
-paths=./settings/mnist*/
-settings=()
-for path in $paths
-do  
-    set=${path%*/}
-    set=${set//"./settings/"}
-    settings+=($set)
-done
-
-for set in ${settings[@]}
-do
-    echo "$set"
-done
+for a in "${alpha[@]}"
+    do
+        for s in "${seed[@]}"
+        do
+            python3 initialize.py --seed $s --dataset mnist --model_name mnist_cnn1 --n_clients 10 --alpha $a
+        done
+    done
