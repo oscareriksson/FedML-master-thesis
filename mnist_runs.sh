4146c496e9961ded9268592170949cfa80fb86b5
oscareriksson <oscareriksson1@hotmail.com>
2022-04-05 10:57:22 +0200

    mnist runs

--- mnist_runs.sh
+++ mnist_runs.sh
@@ -39,33 +39,36 @@ loss_functions=("mse" "ce")
 # done
 
 # FEDED
+# for set in ${settings[@]}
+# do  
+#     for model in ${student_models[@]}
+#     do
+#         python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs --weight_scheme 0
+#     done
+# done
+
+# # FEDED, w1
+# for set in ${settings[@]}
+# do  
+#     for model in ${student_models[@]}
+#     do
+#         python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs --weight_scheme 1
+#     done
+# done
+
+# # FEDED, w2
+# for set in ${settings[@]}
+# do  
+#     for model in ${student_models[@]}
+#     do
+#         for loss in ${loss_functions[@]}
+#         do
+#             python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs_w2 --weight_scheme 2 --autoencoder_epochs $autoencoder_epochs --student_lr 0.00001
+#         done    
+#     done
+# done
+
 for set in ${settings[@]}
 do  
-    for model in ${student_models[@]}
-    do
-        python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs --weight_scheme 0
-    done
+    python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model mnist_cnn2 --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs_w2 --student_loss ce --weight_scheme 2 --autoencoder_epochs $autoencoder_epochs --student_lr 0.00001 
 done
-
-# FEDED, w1
-for set in ${settings[@]}
-do  
-    for model in ${student_models[@]}
-    do
-        python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs --weight_scheme 1
-    done
-done
-
-# FEDED, w2
-for set in ${settings[@]}
-do  
-    for model in ${student_models[@]}
-    do
-        for loss in ${loss_functions[@]}
-        do
-            python3 main.py --settings_file $set --algorithm feded --local_epochs_ensemble $local_epochs_ensemble --student_model $model --public_data_sizes="$public_data_sizes" --client_sample_fraction 1.0 --student_epochs $student_epochs_w2 --weight_scheme 2 --autoencoder_epochs $autoencoder_epochs --student_lr 0.00001
-        done    
-    done
-done
-
-
