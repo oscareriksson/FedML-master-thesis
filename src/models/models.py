3f5aeb5dd2d262bf73c9041d59e3c893313b8350
oscareriksson <oscareriksson1@hotmail.com>
2022-04-12 15:35:27 +0200

    cifar

--- src/models/models.py
+++ src/models/models.py
@@ -251,11 +251,11 @@ class Cifar10_Autoencoder(nn.Module):
         self.encoder_lin = nn.Sequential(
             nn.Linear(3 * 3 * 128, 128),
             nn.ReLU(True),
-            nn.Linear(128, 3)
+            nn.Linear(128, 2)
         )
         
         self.decoder_lin = nn.Sequential(
-            nn.Linear(3, 128),
+            nn.Linear(2, 128),
             nn.ReLU(True),
             nn.Linear(128, 3 * 3 * 128),
             nn.ReLU(True)
