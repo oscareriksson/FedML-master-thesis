8ed47c89224a6f59dd04139959546fd60c1ee913
oscareriksson <oscareriksson1@hotmail.com>
2022-04-20 16:24:36 +0200

    emnist

--- emnist_runs.sh
+++ emnist_runs.sh
@@ -15,7 +15,8 @@ local_model="emnist_cnn1"
 seeds=(1 2 3 4 5)
 alphas=(10.0 1.0 0.1 0.01)
 student_models="emnist_cnn1 emnist_cnn2 emnist_cnn3"
-weight_schemes="0 1 2"
+#weight_schemes="0 1 2"
+weight_schemes="2"
 
 # n_rounds=1
 # local_epochs_ensemble=1
@@ -29,13 +30,13 @@ weight_schemes="0 1 2"
 settings_summary="--dataset $dataset --n_clients $n_clients --public_fraction $public_fraction --distribution niid --local_model $local_model --client_sample_fraction 1.0"
 
 # FEDAVG
-for seed in ${seeds[@]}
-do
-    for alpha in ${alphas[@]}
-    do  
-        python3 main.py $settings_summary --algorithm fedavg --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds
-    done
-done
+# for seed in ${seeds[@]}
+# do
+#     for alpha in ${alphas[@]}
+#     do  
+#         python3 main.py $settings_summary --algorithm fedavg --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds
+#     done
+# done
 
 # # FEDPROX
 # for seed in ${seeds[@]}
@@ -46,14 +47,14 @@ done
 #     done
 # done
 
-# settings_ensemble="--local_epochs_ensemble $local_epochs_ensemble --student_epochs $student_epochs --student_epochs_w2 $student_epochs_w2 --student_lr_w2 $student_lr_w2  --autoencoder_epochs $autoencoder_epochs"
+settings_ensemble="--local_epochs_ensemble $local_epochs_ensemble --student_epochs $student_epochs --student_epochs_w2 $student_epochs_w2 --student_lr_w2 $student_lr_w2  --autoencoder_epochs $autoencoder_epochs"
 
-# # FEDED w0 w1 w2
-# for seed in ${seeds[@]}
-# do
-#     for alpha in ${alphas[@]}
-#     do  
-#         python3 main.py $settings_summary $settings_ensemble --student_models="$student_models" --public_data_sizes="$public_data_sizes" --algorithm feded --student_epochs $student_epochs --seed $seed --alpha $alpha --weight_schemes="$weight_schemes"
-#     done
-# done
+# FEDED w0 w1 w2
+for seed in ${seeds[@]}
+do
+    for alpha in ${alphas[@]}
+    do  
+        python3 main.py $settings_summary $settings_ensemble --student_models="$student_models" --public_data_sizes="$public_data_sizes" --algorithm feded --student_epochs $student_epochs --seed $seed --alpha $alpha --weight_schemes="$weight_schemes"
+    done
+done
 
