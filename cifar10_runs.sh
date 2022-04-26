5d3f3719ef4346cc516b85cb799012113c0a6674
oscareriksson <oscareriksson1@hotmail.com>
2022-04-26 09:39:52 +0200

    runs

--- cifar10_runs.sh
+++ cifar10_runs.sh
@@ -6,16 +6,17 @@ public_fraction=0.5
 n_rounds=100
 local_epochs_ensemble=20
 student_epochs=10
-student_epochs_w2=100
+student_epochs_w2=50
 student_lr_w2=1e-5
 autoencoder_epochs=50
 public_data_sizes="500 1000 5000 10000 25000"
 local_model="cifar10_resnet18"
 
-seeds=(6 7 8 9 10)
+seeds=(0 2 3 4 5 6 7 8 9 10)
 alphas=(10.0 1.0 0.1 0.01)
 student_models="cifar10_resnet18"
-weight_schemes="0 1 2"
+#weight_schemes="0 1 2"
+weight_schemes="2"
 
 # n_rounds=1
 # local_epochs_ensemble=1
@@ -27,26 +28,25 @@ weight_schemes="0 1 2"
 settings_summary="--dataset $dataset --n_clients $n_clients --public_fraction $public_fraction --distribution niid --local_model $local_model --client_sample_fraction 1.0 --train_batch_size 80"
 
 # FEDAVG
-for seed in ${seeds[@]}
-do
-    for alpha in ${alphas[@]}
-    do  
-        python3 main.py $settings_summary --algorithm fedavg --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds
-    done
-done
-
-# # FEDPROX
 # for seed in ${seeds[@]}
 # do
 #     for alpha in ${alphas[@]}
 #     do  
-#         python3 main.py $settings_summary --algorithm fedprox --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds --mu 0.5
+#         python3 main.py $settings_summary --algorithm fedavg --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds
 #     done
 # done
 
+# FEDPROX
+for seed in ${seeds[@]}
+do
+    for alpha in ${alphas[@]}
+    do  
+        python3 main.py $settings_summary --algorithm fedprox --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds --mu 0.1
+    done
+done
+
 settings_ensemble="--local_epochs_ensemble $local_epochs_ensemble --student_epochs $student_epochs --student_epochs_w2 $student_epochs_w2 --student_lr_w2 $student_lr_w2  --autoencoder_epochs $autoencoder_epochs"
 
-seeds=(0 2 3 4 5 6 7 8 9 10)
 # FEDED w0 w1 w2
 for seed in ${seeds[@]}
 do
--- emnist_runs.sh
+++ emnist_runs.sh
@@ -12,11 +12,10 @@ autoencoder_epochs=30
 public_data_sizes="500 1000 5000 30000 60000"
 local_model="emnist_cnn1"
 
-seeds=(1 2 3 4 5)
+seeds=(1 2 3 4 5 6 7 8 9 10)
 alphas=(10.0 1.0 0.1 0.01)
 student_models="emnist_cnn1 emnist_cnn2 emnist_cnn3"
-#weight_schemes="0 1 2"
-weight_schemes="2"
+weight_schemes="0 1 2"
 
 # n_rounds=1
 # local_epochs_ensemble=1
@@ -38,23 +37,23 @@ settings_summary="--dataset $dataset --n_clients $n_clients --public_fraction $p
 #     done
 # done
 
-# # FEDPROX
-# for seed in ${seeds[@]}
-# do
-#     for alpha in ${alphas[@]}
-#     do  
-#         python3 main.py $settings_summary --algorithm fedprox --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds --mu 0.5
-#     done
-# done
-
-settings_ensemble="--local_epochs_ensemble $local_epochs_ensemble --student_epochs $student_epochs --student_epochs_w2 $student_epochs_w2 --student_lr_w2 $student_lr_w2  --autoencoder_epochs $autoencoder_epochs"
-
-# FEDED w0 w1 w2
+# FEDPROX
 for seed in ${seeds[@]}
 do
     for alpha in ${alphas[@]}
     do  
-        python3 main.py $settings_summary $settings_ensemble --student_models="$student_models" --public_data_sizes="$public_data_sizes" --algorithm feded --student_epochs $student_epochs --seed $seed --alpha $alpha --weight_schemes="$weight_schemes"
+        python3 main.py $settings_summary --algorithm fedprox --seed $seed --alpha $alpha --local_epochs 1 --n_rounds $n_rounds --mu 0.1
     done
 done
+
+# settings_ensemble="--local_epochs_ensemble $local_epochs_ensemble --student_epochs $student_epochs --student_epochs_w2 $student_epochs_w2 --student_lr_w2 $student_lr_w2  --autoencoder_epochs $autoencoder_epochs"
+
+# # FEDED w0 w1 w2
+# for seed in ${seeds[@]}
+# do
+#     for alpha in ${alphas[@]}
+#     do  
+#         python3 main.py $settings_summary $settings_ensemble --student_models="$student_models" --public_data_sizes="$public_data_sizes" --algorithm feded --student_epochs $student_epochs --seed $seed --alpha $alpha --weight_schemes="$weight_schemes"
+#     done
+# done
 
